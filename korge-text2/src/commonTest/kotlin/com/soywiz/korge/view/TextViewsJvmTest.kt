acaf0677c780e140981f0f02a620a409f92c8038
soywiz <soywiz@gmail.com>
2023-02-27 13:50:07 +0100

    Add missing test

---
+++ korge-text2/src/commonTest/kotlin/com/soywiz/korge/view/TextViewsJvmTest.kt
@@ -0,0 +1,19 @@
+package com.soywiz.korge.view
+
+import com.soywiz.korge.tests.*
+import com.soywiz.korma.geom.*
+import kotlin.test.*
+
+class TextViewsJvmTest : ViewsForTesting(log = true) {
+    @Test
+    fun textGetBounds1() = viewsTest {
+        val font = views.debugBmpFont
+        assertEquals(MRectangle(0, 0, 77, 8), TextOld("Hello World", font = font, textSize = 8.0).globalBounds)
+    }
+
+    @Test
+    fun textGetBounds2() = viewsTest {
+        val font = views.debugBmpFont
+        assertEquals(MRectangle(0, 0, 154, 16), TextOld("Hello World", font = font, textSize = 16.0).globalBounds)
+    }
+}
