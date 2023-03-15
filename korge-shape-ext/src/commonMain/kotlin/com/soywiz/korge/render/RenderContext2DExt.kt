0a5cc03be0e321dd38c02b0e51f3029722e42258
soywiz <soywiz@gmail.com>
2023-03-15 10:11:19 +0100

    Missing methods

--- korge-shape-ext/src/commonMain/kotlin/com/soywiz/korge/render/RenderContext2DExt.kt
+++ korge-shape-ext/src/commonMain/kotlin/com/soywiz/korge/render/RenderContext2DExt.kt
@@ -1,6 +1,7 @@
 package com.soywiz.korge.render
 
 import com.soywiz.korim.color.RGBA
+import com.soywiz.korma.geom.triangle.TriangleList
 import com.soywiz.korma.geom.vector.VectorPath
 
 fun RenderContext2D.pathFast(path: VectorPath, color: RGBA = this.multiplyColor, filtering: Boolean = this.filtering) {
@@ -9,5 +10,9 @@ fun RenderContext2D.pathFast(path: VectorPath, color: RGBA = this.multiplyColor,
 
 fun RenderContext2D.path(path: VectorPath, color: RGBA = this.multiplyColor, filtering: Boolean = this.filtering, doClipper: Boolean = true) {
     texturedVertexArrayNoTransform(TexturedVertexArray.fromPath(path, color, matrix = m, doClipper = doClipper), filtering)
+}
+
+fun RenderContext2D.triangles(triangles: TriangleList, color: RGBA = this.multiplyColor, filtering: Boolean = this.filtering) {
+    texturedVertexArrayNoTransform(TexturedVertexArray.fromTriangles(triangles, color, matrix = m), filtering)
 }
 
--- korge-shape-ext/src/commonMain/kotlin/com/soywiz/korge/render/TexturedVertexArrayExt.kt
+++ korge-shape-ext/src/commonMain/kotlin/com/soywiz/korge/render/TexturedVertexArrayExt.kt
@@ -3,6 +3,7 @@ package com.soywiz.korge.render
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.color.RGBA
 import com.soywiz.korma.geom.MMatrix
+import com.soywiz.korma.geom.triangle.TriangleList
 import com.soywiz.korma.geom.vector.VectorPath
 import com.soywiz.korma.triangle.poly2tri.triangulateSafe
 
@@ -10,5 +11,11 @@ fun TexturedVertexArray.fromPath(path: VectorPath, colorMul: RGBA = Colors.WHITE
     //return fromTriangles(path.triangulateEarCut(), colorMul, matrix)
     //return fromTriangles(path.triangulatePoly2tri(), colorMul, matrix)
     return TexturedVertexArray.fromTriangles(path.triangulateSafe(doClipper), colorMul, matrix)
+}
+
+fun TexturedVertexArray.fromTriangles(triangles: TriangleList, colorMul: RGBA = Colors.WHITE, matrix: MMatrix? = null): TexturedVertexArray {
+    val tva = TexturedVertexArray(triangles.pointCount, triangles.indices, triangles.numIndices)
+    tva.setSimplePoints(triangles.points, matrix, colorMul)
+    return tva
 }
 
