a88993c199f42cc00a2044b81b089a5cc3fb3a6b
soywiz <soywiz@gmail.com>
2023-05-27 13:25:12 +0200

    Kotlinify tile raycast implementation

--- korma-tile-raycasting/korma-tile-raycasting/src/commonMain/kotlin/korlibs/math/raycasting/RaycastExt.kt
+++ korma-tile-raycasting/korma-tile-raycasting/src/commonMain/kotlin/korlibs/math/raycasting/RaycastExt.kt
@@ -7,7 +7,8 @@ import kotlin.math.*
 // @TODO: Move to KorGE and add companion to Ray, so this can be an static method
 fun RayFromTwoPoints(start: Point, end: Point): Ray = Ray(start, Angle.between(start, end))
 
-private fun sq(f: Float): Float = f * f
+private fun sq(v: Float): Float = v * v
+private fun signumNonZero(v: Float): Float = if (v < 0) -1f else +1f
 
 // https://www.youtube.com/watch?v=NbSee-XM7WA
 fun Ray.firstCollisionInTileMap(
@@ -16,33 +17,24 @@ fun Ray.firstCollisionInTileMap(
     collides: (tilePos: PointInt) -> Boolean
 ): Point? {
     val ray = this
-    val vRayStart = this.point / cellSize
-    val vRayDir = ray.direction.normalized
-    val vRayUnitStepSize = Vector2(
-        sqrt(1f + sq(vRayDir.y / vRayDir.x)),
-        sqrt(1f + sq(vRayDir.x / vRayDir.y)),
+    val rayStart = this.point / cellSize
+    val rayDir = ray.direction.normalized
+    val rayUnitStepSize = Vector2(
+        sqrt(1f + sq(rayDir.y / rayDir.x)),
+        sqrt(1f + sq(rayDir.x / rayDir.y)),
     )
     //println("vRayUnitStepSize=$vRayUnitStepSize")
-    var vMapCheckx = vRayStart.x.toInt()
-    var vMapChecky = vRayStart.y.toInt()
-    var vStepx = 0
-    var vStepy = 0
-    var vRayLength1Dx = 0f
-    var vRayLength1Dy = 0f
-    if (vRayDir.x < 0) {
-        vStepx = -1
-        vRayLength1Dx = (vRayStart.x - (vMapCheckx)) * vRayUnitStepSize.x
-    } else {
-        vStepx = +1
-        vRayLength1Dx = ((vMapCheckx + 1) - vRayStart.x) * vRayUnitStepSize.x
+    var mapCheckX = rayStart.x.toInt()
+    var mapCheckY = rayStart.y.toInt()
+    val stepX = signumNonZero(rayDir.x).toInt()
+    val stepY = signumNonZero(rayDir.y).toInt()
+    var rayLength1Dx = when {
+        rayDir.x < 0 -> (rayStart.x - (mapCheckX)) * rayUnitStepSize.x
+        else -> ((mapCheckX + 1) - rayStart.x) * rayUnitStepSize.x
     }
-
-    if (vRayDir.y < 0) {
-        vStepy = -1
-        vRayLength1Dy = (vRayStart.y - (vMapChecky)) * vRayUnitStepSize.y
-    } else {
-        vStepy = +1
-        vRayLength1Dy = ((vMapChecky + 1) - vRayStart.y) * vRayUnitStepSize.y
+    var rayLength1Dy = when {
+        rayDir.y < 0 -> (rayStart.y - (mapCheckY)) * rayUnitStepSize.y
+        else -> ((mapCheckY + 1) - rayStart.y) * rayUnitStepSize.y
     }
 
     // Perform "Walk" until collision or range check
@@ -51,18 +43,18 @@ fun Ray.firstCollisionInTileMap(
     var fDistance = 0.0f
     while (fDistance < fMaxDistance) {
         // Walk along shortest path
-        if (vRayLength1Dx < vRayLength1Dy) {
-            vMapCheckx += vStepx
-            fDistance = vRayLength1Dx
-            vRayLength1Dx += vRayUnitStepSize.x
+        if (rayLength1Dx < rayLength1Dy) {
+            mapCheckX += stepX
+            fDistance = rayLength1Dx
+            rayLength1Dx += rayUnitStepSize.x
         } else {
-            vMapChecky += vStepy
-            fDistance = vRayLength1Dy
-            vRayLength1Dy += vRayUnitStepSize.y
+            mapCheckY += stepY
+            fDistance = rayLength1Dy
+            rayLength1Dy += rayUnitStepSize.y
         }
 
         // Test tile at new test point
-        if (collides(PointInt(vMapCheckx, vMapChecky))) {
+        if (collides(PointInt(mapCheckX, mapCheckY))) {
             bTileFound = true
             break
         }
@@ -71,7 +63,7 @@ fun Ray.firstCollisionInTileMap(
     // Calculate intersection location
     if (bTileFound) {
         //println("vRayStart=$vRayStart: vRayDir=$vRayDir, fDistance=$fDistance")
-        return (vRayStart + vRayDir * fDistance) * cellSize
+        return (rayStart + rayDir * fDistance) * cellSize
     }
     return null
 }
@@ -81,6 +73,15 @@ fun IStackedIntArray2.raycast(
     cellSize: Size = Size(1f, 1f),
     maxTiles: Int = 10,
     collides: IStackedIntArray2.(tilePos: PointInt) -> Boolean
+): Point? {
+    return ray.firstCollisionInTileMap(cellSize, maxTiles) { pos -> collides(this, pos) }
+}
+
+fun IntIArray2.raycast(
+    ray: Ray,
+    cellSize: Size = Size(1f, 1f),
+    maxTiles: Int = 10,
+    collides: IntIArray2.(tilePos: PointInt) -> Boolean
 ): Point? {
     return ray.firstCollisionInTileMap(cellSize, maxTiles) { pos -> collides(this, pos) }
 }
